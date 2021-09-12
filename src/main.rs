ed3eba5106ba64a8398b3b600b08d9cc516520fe
runzehao <runzehao@creditease.cn>
2021-09-13 00:18:17 +0800

    optimize

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,69 @@
-use std::sync::{mpsc, Mutex, Arc};
+use std::sync::{Mutex, Arc};
 use std::thread;
 use std::time::Duration;
 use std::ops::Div;
 use serde_json::Value;
-use std::sync::mpsc::Sender;
-use std::future::Future;
-use futures::executor::block_on;
+use tokio::runtime::Runtime;
+use chrono::Local;
 
-#[tokio::main]
-async fn main() {
-    let mut db = Db {
+// 跟踪价格 时间间隔
+const TRACK_PRICE_INTERVAL: i32 = 10;
+// 跟踪次数
+const TRACK_PRICE_CNT: i32 = 2;
+// TWITTER USER ID
+const TWITTER_ID: i64 = 123;
+
+
+fn main() {
+    let db = Db {
         suc_cnt: 0,
         suc_rate: 0.0,
         last_time: 0,
         tweets: Vec::new(),
     };
 
-    let suc_rate = db.suc_rate;
     let mtx = Arc::new(Mutex::new(db));
-    loop {
-        let tweet = tweet_published();
-        println!("tweet published:{}, estimated success rate:{}", tweet.content, suc_rate);
-        let lock = Arc::clone(&mtx);
 
-        // thread::spawn(move||{
-            let fut = monitor_and_alarm(lock, tweet);
-            block_on(fut);
-        // });
+    let mut result_list = Vec::new();
+
+    loop {
+        //扫描是否有新的tweet发布
+
+        let tweet = tweet_published();
+        let lock = Arc::clone(&mtx);
+        println!("[tid:{}]tweet published, estimated success rate:{:.3}%", tweet.time, lock.lock().unwrap().suc_rate * 100.0);
+
+
+        //新建线程去处理每个新的tweet
+        let ans = thread::spawn(move || {
+            monitor_and_alarm(lock, tweet);
+        });
+        result_list.push(ans);
     }
 }
 
-async fn monitor_and_alarm(lock:Arc<Mutex<Db>>, tweet:Tweet){
+fn monitor_and_alarm(lock: Arc<Mutex<Db>>, tweet: Tweet) {
+    let range = Runtime::new()
+        .expect("Failed to create runtime")
+        .block_on(output(&(tweet.time)));
 
-    let range = output().await;
     let mut db = lock.lock().unwrap();
-    db.add(tweet,range.ge(&0.0));
+    db.add(tweet, range.ge(&0.0));
+    println!("{:#?}", db);
 }
 
-async fn output() ->f64{
-    println!("try to get base data...");
+async fn output(tid: &i64) -> f64 {
+    println!("[tid:{}]try to get base data...", tid);
     let btc_tick = get_btc_price().await.unwrap();
-    println!("base_price:{},base_time:{}", btc_tick.price, btc_tick.ts);
-    track_price(btc_tick.price, 2).await
-
+    println!("[tid:{}]base_price:{},base_time:{}", tid, btc_tick.price, btc_tick.ts);
+    track_price(tid, btc_tick.price, TRACK_PRICE_CNT).await
 }
 
-async fn track_price(base_price: f64, times: i32) -> f64 {
+async fn track_price(tid: &i64, base_price: f64, times: i32) -> f64 {
     let mut range = 0.0;
-    for i in 0..times {
-        println!("track btc price, waiting for 30s...");
-        thread::sleep(Duration::from_secs(30));
+    for _i in 0..times {
+        println!("[tid:{}]track btc price, waiting for {}s...", tid, TRACK_PRICE_INTERVAL);
+        thread::sleep(Duration::from_secs(TRACK_PRICE_INTERVAL as u64));
         let btc_tick = get_btc_price().await;
         if let Err(str) = btc_tick {
             println!("{}", str);
@@ -57,7 +71,7 @@ async fn track_price(base_price: f64, times: i32) -> f64 {
         }
         let btc_tick = btc_tick.unwrap();
         range = (btc_tick.price - base_price).div(base_price);
-        println!("price:{},time:{}, range:{:.5}%", btc_tick.price, btc_tick.ts, range * 100.0);
+        println!("[tid:{}]price:{},time:{}, range:{:.5}%", tid, btc_tick.price, btc_tick.ts, range * 100.0);
     }
     range
 }
@@ -77,14 +91,12 @@ async fn get_btc_price() -> Result<BtcTick, String> {
 }
 
 fn tweet_published() -> Tweet {
-    println!("monitor tweets....");
-    thread::sleep(Duration::from_secs(5));
+    thread::sleep(Duration::from_secs(20));
     Tweet {
-        time: 10,
+        time: Local::now().timestamp_millis(),
         content: "asd".to_string(),
     }
 }
-
 
 
 pub struct BtcTick {
@@ -92,11 +104,13 @@ pub struct BtcTick {
     price: f64,
 }
 
+#[derive(Debug)]
 pub struct Tweet {
     time: i64,
     content: String,
 }
 
+#[derive(Debug)]
 pub struct Db {
     pub last_time: i64,
     pub suc_rate: f64,
@@ -105,7 +119,7 @@ pub struct Db {
 }
 
 impl Db {
-    pub fn add(&mut self, t: Tweet, result:bool) {
+    pub fn add(&mut self, t: Tweet, result: bool) {
         self.last_time = t.time;
         self.tweets.push(t);
 
@@ -114,7 +128,6 @@ impl Db {
         }
         self.suc_rate = self.suc_cnt as f64 / self.tweets.len() as f64;
     }
-
 }
 
 
